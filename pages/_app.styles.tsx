757344b2e53cfee11bb5b9eeacd1a546507cc2da
Ruei <ruei@null.computer>
2022-04-13 00:20:52 +0800

    separate style file for _app.tsx :truck:

---
+++ pages/_app.styles.tsx
@@ -0,0 +1,24 @@
+import { createStyles } from '@mantine/core';
+
+const useStyles = createStyles(theme => ({
+  wrapper: {
+    backgroundColor: 'var(--main-bg-color)'
+  },
+
+  main: {
+    margin: '0 auto',
+    maxWidth: 'var(--wrapper-max-width)',
+    minHeight: `calc(100vh - var(--header-height))`
+  },
+
+  content: {
+    marginLeft: 'var(--side-nav-width)',
+    padding: '40px 20px',
+
+    [theme.fn.smallerThan('md')]: {
+      '--side-nav-width': 0
+    }
+  }
+}));
+
+export default useStyles;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,37 +2,16 @@ import type { AppProps } from 'next/app';
 import { useState } from 'react';
 import { MantineProvider, createStyles } from '@mantine/core';
 import { useScrollLock } from '@mantine/hooks';
+import useStyles from './_app.styles';
 import GlobalStyle from '../components/Mantine/GlobalStyle';
 import Header from '../components/Header/Header';
 import SideNavbar from '../components/SideNavbar/SideNavbar';
-
-const useStyles = createStyles(theme => ({
-  wrapper: {
-    backgroundColor: 'var(--main-bg-color)'
-  },
-
-  main: {
-    margin: '0 auto',
-    maxWidth: 'var(--wrapper-max-width)',
-    minHeight: `calc(100vh - var(--header-height))`
-  },
-
-  content: {
-    marginLeft: 'var(--side-nav-width)',
-    padding: '40px 20px',
-
-    [theme.fn.smallerThan('md')]: {
-      '--side-nav-width': 0
-    }
-  }
-}));
 
 function MyApp({ Component, pageProps }: AppProps) {
   // navigation open state
   const [opened, setOpened] = useState<boolean>(false);
   const { classes } = useStyles();
   const [scrollLocked, setScrollLocked] = useScrollLock();
-  // const matchLargeScreen = useMediaQuery(`(min-width: ${theme.breakpoints.md}px)`);
 
   return (
     <MantineProvider withNormalizeCSS withGlobalStyles>
