39a9447844e822c09817a9fd63dacf2df00726cc
Ruei <ruei@null.computer>
2022-04-14 21:56:01 +0800

    create custom document for render style tag on server side :lipstick:

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { DocumentContext } from 'next/document';
+import { ServerStyles, createStylesServer } from '@mantine/next';
+
+const stylesServer = createStylesServer();
+
+class _Document extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <ServerStyles html={initialProps.html} server={stylesServer} />
+        </>
+      )
+    };
+  }
+}
+
+export default _Document;
